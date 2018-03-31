a3ea6782d0862b6a0c101e86860ad33c400369ef
Dylan Bulmer <dylanmbulmer@gmail.com>
2018-03-31 09:49:17 -0400

    Moving to HTTPS

--- Website/app.js
+++ Website/app.js
@@ -143,21 +143,72 @@ if (data.https) {
     // HTTPS Settings
     app.set('port', 443);
 
-    require('greenlock-express').create({
-
+    // returns an instance of node-greenlock with additional helper methods
+    var lex = require('greenlock-express').create({
+        // set to https://acme-v01.api.letsencrypt.org/directory in production
         server: 'staging',
-        email: 'piggahbro@gmail.com',
-        agreeTos: true,
-        approvedDomains: ['piggahbrostudios.com', 'www.piggahbrostudios.com', 'blog.piggahbrostudios.com', 'account.piggahbrostudios.com', 'gaming.piggahbrostudios.com', 'store.piggahbrostudios.com'],
-        app: app,
+
+        // If you wish to replace the default plugins, you may do so here
+        //
+        challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) },
+        store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' }),
+
+        // You probably wouldn't need to replace the default sni handler
+        // See https://git.daplie.com/Daplie/le-sni-auto if you think you do
+        //, sni: require('le-sni-auto').create({})
+
+        approveDomains: approveDomains,
+
         renewWithin: (91 * 24 * 60 * 60 * 1000),
         renewBy: (90 * 24 * 60 * 60 * 1000),
         debug: true
+    });
 
-    }).listen(80, 443);
+    require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+        console.log("Listening for ACME http-01 challenges on", this.address());
+    });
+
+    require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+        console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+    });
+
+    //require('greenlock-express').create({
+
+    //    server: 'staging',
+    //    email: 'piggahbro@gmail.com',
+    //    agreeTos: true,
+    //    approvedDomains: ['piggahbrostudios.com', 'www.piggahbrostudios.com', 'blog.piggahbrostudios.com', 'account.piggahbrostudios.com', 'gaming.piggahbrostudios.com', 'store.piggahbrostudios.com'],
+    //    app: app,
+    //    renewWithin: (91 * 24 * 60 * 60 * 1000),
+    //    renewBy: (90 * 24 * 60 * 60 * 1000),
+    //    debug: true
+
+    //}).listen(80, 443);
 } else {
     // HTTP Settings
     app.set('port', 80);
 
     app.listen(app.get('port'));
+}
+
+function approveDomains(opts, certs, cb) {
+    // This is where you check your database and associated
+    // email addresses with domains and agreements and such
+
+
+    // The domains being approved for the first time are listed in opts.domains
+    // Certs being renewed are listed in certs.altnames
+    if (certs) {
+        opts.domains = certs.altnames;
+    }
+    else {
+        opts.email = 'piggahbro@gmail.com';
+        opts.agreeTos = true;
+    }
+
+    // NOTE: you can also change other options such as `challengeType` and `challenge`
+    // opts.challengeType = 'http-01';
+    // opts.challenge = require('le-challenge-fs').create({});
+
+    cb(null, { options: opts, certs: certs });
 }
