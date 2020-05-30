e09b6647d77a8d196ede38a1e62eea56983e37b0
Dylan Bulmer <dylanmbulmer@gmail.com>
2020-05-30 12:32:50 -0400

    fix environment configuration

---
+++ web/serve.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const {NODE_ENV} = process.env
+
+switch(NODE_ENV) {
+    case "production":
+        require('./https');
+        break;
+    case "development":
+        require('./app');
+        break;
+    default:
+        console.log('NODE_ENV is required but was not specified.');
+}
